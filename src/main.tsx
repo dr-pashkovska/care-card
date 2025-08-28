4807dbd812133b203bcbbc7ad70b17f790eda37e
Roman Zuboley <xenusrom@gmail.com>
2025-08-28 22:37:23 +0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.scss";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <div>Afa</div>
+  </StrictMode>
+);
