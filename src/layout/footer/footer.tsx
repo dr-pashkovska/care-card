b33599bdeb516de66ddcbc2f994e752775735d45
Roman Zuboley <xenusrom@gmail.com>
2025-09-11 14:21:44 +0300

    all data

--- src/layout/footer/footer.tsx
+++ src/layout/footer/footer.tsx
@@ -7,19 +7,21 @@ export const Footer: FC = () => {
   return (
     <footer>
       <Section>
-        <p>Готові отримати зрозумілий план догляду?</p>
-        <p>Ніяких хаотичних покупок, лише чітка система, яка працює саме для вас.</p>
+        <p className="text-center text-xl text-slate-600">Готові отримати зрозумілий план догляду?</p>
+        <p className="text-center text-xl text-slate-600">
+          Ніяких хаотичних покупок, лише чітка система, яка працює саме для вас.
+        </p>
         <button
           type="button"
           className={cn([
             'transition-all cursor-pointer shadow-md hover:shadow-lg',
             'sm:py-4 sm:px-8 py-3 px-5',
-            // 'bg-gradient-to-tr from-orange-300/70 to-orange-300',
             'bg-orange-400/70 hover:bg-orange-400/80',
             'text-white hover:text-gray-100',
             'sm:text-2xl text-lg font-normal',
             'rounded-lg border-none',
             'focus:outline-none',
+            'sm:max-w-2/3 w-full mx-auto',
           ])}
         >
           Записатися на консультацію
--- src/layout/sections/consultation-types.tsx
+++ src/layout/sections/consultation-types.tsx
@@ -1,4 +1,5 @@
-import type { FC } from 'react'
+import cn from 'classnames'
+import type { FC, ReactNode } from 'react'
 
 import { SectionHeader } from '~/components/section-header'
 
@@ -9,45 +10,76 @@ export const ConsultationTypes: FC = () => {
     <Section>
       <SectionHeader title="Види консультацій" />
 
-      <div className="text-lg text-slate-700 flex flex-col gap-2">
-        <h3>Консультація без супроводу — 50 €</h3>
-        <ul className="text-lg text-slate-700 flex flex-col gap-2">
-          <li>- Індивідуальний файл-протокол (ранок/вечір, частота, тривалість).</li>
-          <li>- Аналіз ваших засобів (що залишити / що замінити).</li>
-          <li>- Підбір нових засобів із прикладами (до 3 варіантів на кожен).</li>
-          <li>- 3 дні для уточнюючих питань у Telegram.</li>
-        </ul>
-      </div>
-
-      <div className="text-lg text-slate-700 flex flex-col gap-2">
-        <h3>Консультація із супроводом 3 місяці — 100 €</h3>
-        <ul className="text-lg text-slate-700 flex flex-col gap-2">
-          <li>
-            - Усе з пакету <b>без супроводу</b>.
-          </li>
-          <li>- 3 місяці супроводу у Telegram.</li>
-          <li>- 1–2 рази на тиждень повноцінні відповіді.</li>
-          <li>- Контроль введення активів (ретинол, кислоти тощо).</li>
-          <li>- Корекція догляду за потреби.</li>
-          <li>- Сезонні заміни та адаптація протоколу.</li>
-        </ul>
-      </div>
-
-      <div className="text-lg text-slate-700 flex flex-col gap-2">
-        <h3>
-          Річний супровід — 250 € (рекомендований пацієнтам на системному лікуванні із хронічними дерматологічними
-          станами)
-        </h3>
-        <ul className="text-lg text-slate-700 flex flex-col gap-2">
-          <li>
-            - Усе з пакету <b>3 місяці</b>.
-          </li>
-          <li>- 12 місяців супроводу.</li>
-          <li>- Регулярний контроль результатів і адаптація догляду.</li>
-          <li>- Підтримка під час системної терапії.</li>
-          <li>- Запобігання «зривам» та хаотичним покупкам: готові списки альтернатив.</li>
-        </ul>
-      </div>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        <Block title="Консультація без супроводу" list={PACK_50} price={50} />
+        <Block title="Консультація із супроводом 3 місяці" list={PACK_100} price={100} />
+        <Block
+          description="(рекомендований пацієнтам на системному лікуванні із хронічними дерматологічними станами)"
+          title="Річний супровід"
+          list={PACK_250}
+          price={250}
+        />
+      </ul>
     </Section>
   )
 }
+
+const Block: FC<{ title: ReactNode; list: (ReactNode | string)[]; price: number; description?: string }> = ({
+  title,
+  list,
+  price,
+  description,
+}) => {
+  return (
+    <li
+      className={cn([
+        'border border-teal-600/40 rounded-md p-4',
+        'text-slate-700 bg-white/40 shadow-md',
+        'flex flex-col gap-4',
+      ])}
+    >
+      <span className="text-5xl font-bold text-center text-teal-600">{price} €</span>
+      <h4 className="text-xl text-center">{title}</h4>
+      <div className="border-b border-b-slate-200 w-1/3 mx-auto" />
+      {description && <span className="text-sm text-slate-500 leading-4">{description}</span>}
+      <ul>
+        {list.map((item, key) => (
+          <li key={key} className="flex gap-3 pb-3">
+            <div className="py-1.5">
+              <span className="block rounded-full p-1.5 bg-teal-500" />
+            </div>
+            <span className="text-base text-slate-700">{item}</span>
+          </li>
+        ))}
+      </ul>
+    </li>
+  )
+}
+
+const PACK_50 = [
+  'Індивідуальний файл-протокол (ранок/вечір, частота, тривалість).',
+  'Аналіз ваших засобів (що залишити / що замінити).',
+  'Підбір нових засобів із прикладами (до 3 варіантів на кожен).',
+  '3 дні для уточнюючих питань у Telegram.',
+]
+
+const PACK_100 = [
+  <>
+    Усе з пакету <b>без супроводу</b>.
+  </>,
+  '3 місяці супроводу у Telegram.',
+  '1–2 рази на тиждень повноцінні відповіді.',
+  'Контроль введення активів (ретинол, кислоти тощо).',
+  'Корекція догляду за потреби.',
+  'Сезонні заміни та адаптація протоколу.',
+]
+
+const PACK_250 = [
+  <>
+    Усе з пакету <b>3 місяці</b>.
+  </>,
+  '12 місяців супроводу.',
+  'Регулярний контроль результатів і адаптація догляду.',
+  'Підтримка під час системної терапії.',
+  'Запобігання «зривам» та хаотичним покупкам: готові списки альтернатив.',
+]
